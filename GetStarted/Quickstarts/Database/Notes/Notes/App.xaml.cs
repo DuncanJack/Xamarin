bd0bfdb699e4f6d2bdb4794164dda0e3573cb334
= <=>
2019-12-27 10:31:06 +0930

    Complete Database quickstart

---
+++ GetStarted/Quickstarts/Database/Notes/Notes/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Notes.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes
+{
+    public partial class App : Application
+    {
+        static NoteDatabase _database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if(_database == null)
+                {
+                    var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3");
+                    _database = new NoteDatabase(path);
+                }
+                return _database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
