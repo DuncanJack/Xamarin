38ecc703c7de91f6f0b4a85bce2b65a3d5873d6d
= <=>
2019-12-28 18:02:32 +0930

    Complete Grid tutorial

---
+++ GetStarted/Tutorials/GridTutorial/GridTutorial/B.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GridTutorial
+{
+    public partial class B : ContentPage
+    {
+        public B()
+        {
+            InitializeComponent();
+        }
+    }
+}
