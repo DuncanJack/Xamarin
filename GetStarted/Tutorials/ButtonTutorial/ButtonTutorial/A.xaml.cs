e072fce182ec63740e3f2197ddd31cdd5911cc3a
= <=>
2019-12-29 10:32:06 +0930

    Complete Button tutorial

---
+++ GetStarted/Tutorials/ButtonTutorial/ButtonTutorial/A.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ButtonTutorial
+{
+    public partial class A : ContentPage
+    {
+        public A()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(System.Object sender, System.EventArgs e)
+        {
+            ((Button)sender).Text = "Click me again!";
+        }
+    }
+}
---
+++ GetStarted/Tutorials/ButtonTutorial/ButtonTutorial/B.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ButtonTutorial
+{
+    public partial class B : ContentPage
+    {
+        public B()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Time: {DateTime.Now.ToLongTimeString()}");
+        }
+    }
+}
