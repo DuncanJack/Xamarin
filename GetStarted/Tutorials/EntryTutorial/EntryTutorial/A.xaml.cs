4790abd007d48668b1b113bf86e63c26ab3fb32f
= <=>
2019-12-29 10:57:21 +0930

    Complete Entry tutorial

---
+++ GetStarted/Tutorials/EntryTutorial/EntryTutorial/A.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EntryTutorial
+{
+    public partial class A : ContentPage
+    {
+        public A()
+        {
+            InitializeComponent();
+        }
+
+        void OnEntryTextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(System.Object sender, System.EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
