e26984c0061b6de57a4e07d10acd0aebfdb0f005
= <=>
2019-12-29 18:27:12 +1000

    Complete Editor tutorial

---
+++ GetStarted/Tutorials/EditorTutorial/EditorTutorial/A.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EditorTutorial
+{
+    public partial class A : ContentPage
+    {
+        public A()
+        {
+            InitializeComponent();
+        }
+
+        void OnEditorTextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEditorCompleted(System.Object sender, System.EventArgs e)
+        {
+            string text = ((Editor)sender).Text;
+        }
+    }
+}
