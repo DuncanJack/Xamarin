df6547ecfa56cf56ae733071a5133380fb7ccf38
= <=>
2019-12-29 10:13:18 +0930

    Complete Label tutorial

---
+++ GetStarted/Tutorials/LabelTutorial/LabelTutorial/B.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LabelTutorial
+{
+    public partial class B : ContentPage
+    {
+        public B()
+        {
+            InitializeComponent();
+        }
+    }
+}
