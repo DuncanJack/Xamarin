fa1b7ea19ae616db856a881fe2502a12afb34368
= <=>
2019-12-30 10:55:37 +1000

    Complete ListView tutorial

---
+++ GetStarted/Tutorials/ListViewTutorial/ListViewTutorial/B.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ListViewTutorial.Models;
+using Xamarin.Forms;
+
+namespace ListViewTutorial
+{
+    public partial class B : ContentPage
+    {
+        public IEnumerable<Monkey> Monkeys
+        {
+            get
+            {
+                return Zoo.Monkeys.OrderBy(x => x.Name);
+            }
+        }
+
+        public B()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        void OnListViewItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            Monkey selectedItem = e.SelectedItem as Monkey;
+        }
+
+        void OnListViewItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            Monkey tappedItem = e.Item as Monkey;
+        }
+    }
+}
---
+++ GetStarted/Tutorials/ListViewTutorial/ListViewTutorial/C.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ListViewTutorial.Models;
+using Xamarin.Forms;
+
+namespace ListViewTutorial
+{
+    public partial class C : ContentPage
+    {
+        public IEnumerable<Monkey> Monkeys
+        {
+            get
+            {
+                return Zoo.Monkeys.OrderBy(x => x.Name);
+            }
+        }
+
+        public C()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        void OnListViewItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            Monkey selectedItem = e.SelectedItem as Monkey;
+        }
+
+        void OnListViewItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            Monkey tappedItem = e.Item as Monkey;
+        }
+    }
+}
