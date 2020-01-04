5bb9406944f1e0a0ea0d0a62b25b33659803cb0b
= <=>
2020-01-04 11:27:55 +1000

    Complete Property Elements

---
+++ Xamarin.Forms/XAML/XAMLBasics/EssentialSyntax/EssentialSyntax/PropertyElements.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EssentialSyntax
+{
+    public partial class PropertyElements : ContentPage
+    {
+        public PropertyElements()
+        {
+            InitializeComponent();
+        }
+    }
+}
