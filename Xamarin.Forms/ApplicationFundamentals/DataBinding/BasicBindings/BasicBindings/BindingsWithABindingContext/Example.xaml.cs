e8c38cce5ab7a285f865f3f4716d1d6497aa57e3
= <=>
2020-01-09 17:08:34 +1000

    Complete Basic Bindings

---
+++ Xamarin.Forms/ApplicationFundamentals/DataBinding/BasicBindings/BasicBindings/BindingsWithABindingContext/Example.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BasicBindings.BindingsWithABindingContext
+{
+    public partial class Example : ContentPage
+    {
+        public Example()
+        {
+            InitializeComponent();
+
+            // sliderB
+            labelB.BindingContext = sliderB;
+            labelB.SetBinding(RotationProperty, "Value");
+        }
+
+        void sliderA_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var slider = sender as Slider;
+            labelA.Rotation = slider.Value;
+        }
+    }
+}
