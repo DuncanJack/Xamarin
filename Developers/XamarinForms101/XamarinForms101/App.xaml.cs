ac47de594d54729e9789a39172695ff6f0414d38
= <=>
2020-01-14 19:11:24 +0930

    Complete Data Binding

--- Developers/XamarinForms101/XamarinForms101/App.xaml.cs
+++ Developers/XamarinForms101/XamarinForms101/App.xaml.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using XamarinForms101.Services;
-using XamarinForms101.Views;
 
 namespace XamarinForms101
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
         }
 
---
+++ Developers/XamarinForms101/XamarinForms101/DataBinding/DataBindingViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace XamarinForms101.DataBinding
+{
+    public class DataBindingViewModel : INotifyPropertyChanged
+    {
+        string _name = string.Empty;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if(value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                    OnPropertyChanged(nameof(DisplayName));
+                }
+            }
+        }
+
+        public string DisplayName => $"Name Entered: {Name}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
