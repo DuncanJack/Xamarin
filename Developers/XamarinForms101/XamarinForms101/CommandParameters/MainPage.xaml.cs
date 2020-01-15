fed7197d460205f25c301d0000f57db324c1fcde
= <=>
2020-01-15 19:28:35 +0930

    Component Parameters

---
+++ Developers/XamarinForms101/XamarinForms101/CommandParameters/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms101.CommandParameters
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPageViewModel VM => (MainPageViewModel)BindingContext;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            VM.CommandD.Execute(null);
+        }
+    }
+}
---
+++ Developers/XamarinForms101/XamarinForms101/CommandParameters/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinForms101.CommandParameters
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            CommandA = new Command(() =>
+                Application.Current.MainPage.DisplayAlert("A", "a", "OK"));
+
+            CommandB = new Command<string>((p) =>
+                Application.Current.MainPage.DisplayAlert("B", p, "OK"));
+
+            CommandC = new Command(() =>
+                Application.Current.MainPage.DisplayAlert("C", "c", "OK"));
+
+            CommandD = new Command(() =>
+                Console.WriteLine("CommandD"));
+        }
+
+        public ICommand CommandA { get; set; }
+        public ICommand CommandB { get; set; }
+        public ICommand CommandC { get; set; }
+
+        public ICommand CommandD { get; set; }
+    }
+}
