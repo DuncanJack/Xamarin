29de60e038a6964f70236e555b01b646855b11de
duncanjack <duncan@openword.co.uk>
2020-01-16 19:02:28 +0930

    Application Resources

---
+++ Developers/XamarinForms101/XamarinForms101/ApplicationResources/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms101.ApplicationResources
+{
+    public partial class MainPage : ContentPage
+    {
+        public int PageResourceCount => Resources.Count;
+        public int AppResourceCount => App.Current.Resources.Count;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            label1.Text = $"PageResourceCount: {PageResourceCount}";
+            label2.Text = $"AppResourceCount: {AppResourceCount}";
+        }
+    }
+}
