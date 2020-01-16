902fe26cf3e599f840df4a7f830e82cf1b977d83
duncanjack <duncan@openword.co.uk>
2020-01-16 19:14:46 +0930

    Dynamic Resources

---
+++ Developers/XamarinForms101/XamarinForms101/DynamicResources/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms101.DynamicResources
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Resources["MyColor"] = Color.Red;
+        }
+    }
+}
