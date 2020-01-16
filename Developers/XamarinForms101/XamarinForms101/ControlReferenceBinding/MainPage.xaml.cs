e9a58c12d49dd5933f407159896fb7d15fa7fece
duncanjack <duncan@openword.co.uk>
2020-01-16 18:21:19 +0930

    Control Reference Binding

---
+++ Developers/XamarinForms101/XamarinForms101/ControlReferenceBinding/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms101.ControlReferenceBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Developers/XamarinForms101/XamarinForms101/ControlReferenceBinding/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinForms101.ControlReferenceBinding
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            MyCommand = new Command<string>((s) =>
+            {
+                Console.WriteLine(s);
+            });
+        }
+
+        public ICommand MyCommand { get; set; }
+    }
+}
