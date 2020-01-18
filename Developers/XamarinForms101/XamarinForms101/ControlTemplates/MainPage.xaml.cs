6c93852b015c02ae878113401a3836b7e22e6275
duncanjack <duncan@openword.co.uk>
2020-01-18 12:18:20 +0930

    Control Templates

---
+++ Developers/XamarinForms101/XamarinForms101/ControlTemplates/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms101.ControlTemplates
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
