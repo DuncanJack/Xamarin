ae6fbd4e9c6087a568494f3abc0a1d65b70dfa4e
duncanjack <duncan@openword.co.uk>
2020-01-20 18:57:39 +0930

    AppLifecycle

---
+++ Docs/Xamarin.Forms/ApplicationFundamentals/AppLifecycle/AppLifecycle/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLifecycle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MyTabbedPage();
+
+            PageAppearing += (object sender, Page e) => Console.WriteLine($"PageAppearing: {e.Title}");
+            PageDisappearing += (object sender, Page e) => Console.WriteLine($"PageDisppearing: {e.Title}");
+
+            ModalPushing += (object sender, ModalPushingEventArgs e) => Console.WriteLine($"ModalPushing, triggered by: {e.Modal.Title}");
+            ModalPushed += (object sender, ModalPushedEventArgs e) => Console.WriteLine($"ModalPushed, triggered by: {e.Modal.Title}");
+
+            ModalPopping += (object sender, ModalPoppingEventArgs e) => Console.WriteLine($"ModalPopping, triggered by: {e.Modal.Title}");
+            ModalPopped += (object sender, ModalPoppedEventArgs e) => Console.WriteLine($"ModalPopped, triggered by: {e.Modal.Title}");
+        }
+
+        private void App_ModalPopped(object sender, ModalPoppedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Docs/Xamarin.Forms/ApplicationFundamentals/AppLifecycle/AppLifecycle/MyPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AppLifecycle
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        async void PushButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPage());
+        }
+
+        async void PushModalButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MyPage { Title = "MyPage Modal" });
+        }
+
+        async void PopModalButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (Title == "MyPage Modal")
+                await Navigation.PopModalAsync();
+        }
+
+        async void XToolbarItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ContentPage { Title = "X" });
+        }
+
+        async void YToolbarItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ContentPage { Title = "Y" });
+        }
+
+        async void ZToolbarItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ContentPage { Title = "Z" });
+        }
+    }
+}
