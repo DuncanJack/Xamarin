3feef381bd018ad66717880d669b5605b4d79992
duncanjack <duncan@openword.co.uk>
2020-01-20 17:47:36 +0930

    AppClass

---
+++ Docs/Xamarin.Forms/ApplicationFundamentals/AppClass/AppClass/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppClass
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Console.WriteLine("App");
+
+            InitializeComponent();
+
+            MainPage = new MainPage { Title = "AppClass" };
+            App.Current.Properties["Constructed"] = DateTime.Now.ToLongTimeString();
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine($"App.OnStart, Constructed: {App.Current.Properties["Constructed"]}");
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine($"App.OnSleep, Constructed: {App.Current.Properties["Constructed"]}");
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine($"App.OnResume, Constructed: {App.Current.Properties["Constructed"]}");
+        }
+    }
+}
---
+++ Docs/Xamarin.Forms/ApplicationFundamentals/AppClass/AppClass/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppClass
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LogButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine($"LogButton_Clicked, Constructed: {App.Current.Properties["Constructed"]}");
+        }
+    }
+}
