6ed412e8c8e8c59bd46b591f116207d30e990800
duncanjack <duncan@openword.co.uk>
2020-01-25 08:55:47 +0930

    Pixels and Device-Independent Units

---
+++ Docs/Xamarin.Forms/Graphics/DrawingBasics/PixelsAndDeviceIndependentUnits/PixelsAndDeviceIndependentUnits/EllipseFillPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace PixelsAndDeviceIndependentUnits
+{
+    public class EllipseFillPage : ContentPage
+    {
+        SKCanvasView canvasView;
+
+        public EllipseFillPage()
+        {
+            canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            float strokeWidth = 50;
+            float xRadius = (info.Width - strokeWidth) / 2;
+            float yRadius = (info.Height - strokeWidth) / 2;
+
+            SKPaint paint = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = strokeWidth
+            };
+
+            canvas.DrawOval(info.Width / 2, info.Height / 2, xRadius, yRadius, paint);
+        }
+    }
+}
+
