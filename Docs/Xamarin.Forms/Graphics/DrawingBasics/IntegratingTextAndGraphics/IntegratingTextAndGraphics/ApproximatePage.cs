8098e936f68a5f8ef3062139c37d6c1e768061d5
duncanjack <duncan@openword.co.uk>
2020-01-27 12:11:14 +0930

    Integrating Text and Graphics

---
+++ Docs/Xamarin.Forms/Graphics/DrawingBasics/IntegratingTextAndGraphics/IntegratingTextAndGraphics/ApproximatePage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace IntegratingTextAndGraphics
+{
+    public class ApproximatePage : ContentPage
+    {
+        public ApproximatePage()
+        {
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.AliceBlue);
+
+            string text = "Hello World";
+
+            SKPaint paint = new SKPaint
+            {
+                IsAntialias = true,
+                Color = SKColors.DarkRed,
+                Style = SKPaintStyle.Stroke,
+                TextSize = 100f,
+                TextAlign = SKTextAlign.Center
+            };
+
+            canvas.DrawText(text, info.Width / 2f, info.Height / 2f, paint);
+
+        }
+    }
+}
+
