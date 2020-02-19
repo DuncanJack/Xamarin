48fc86d61d067205e43f1e73cee37c7f98128271
duncanjack <duncan@openword.co.uk>
2020-02-19 17:56:09 +0930

    Finger Painting

---
+++ Docs/Xamarin.Forms/Graphics/LinesAndPaths/FingerPainting/FingerPainting/TouchActionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FingerPainting
+{
+    public enum TouchActionType
+    {
+        Entered,
+        Pressed,
+        Moved,
+        Released,
+        Exited,
+        Cancelled
+    }
+}
