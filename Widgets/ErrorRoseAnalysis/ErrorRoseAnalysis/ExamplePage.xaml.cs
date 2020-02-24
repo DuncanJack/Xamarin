47fb25e1ae85894fa61e738488b0ea2f5a20a7b9
duncanjack <duncan@openword.co.uk>
2020-02-24 20:01:17 +0930

    Starting Error Rose Analysis

---
+++ Widgets/ErrorRoseAnalysis/ErrorRoseAnalysis/ExamplePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace ErrorRoseAnalysis
+{
+    public partial class ExamplePage : ContentPage
+    {
+        public ExamplePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.PowderBlue
+            };
+
+            SKRect rect = new SKRect(50f, 50f, 100f, 100f);
+
+            canvas.DrawRect(rect, paint);
+        }
+
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+
+            slider.Value = 3f;
+
+            Console.WriteLine(value);
+        }
+    }
+}
