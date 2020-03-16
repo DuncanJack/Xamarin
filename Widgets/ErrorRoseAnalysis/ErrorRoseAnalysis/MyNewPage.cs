a4359a808cc6f68ea2742e72760950507c13685c
duncanjack <duncan@openword.co.uk>
2020-03-16 19:43:37 +0930

    Grid changes

--- Widgets/ErrorRoseAnalysis/ErrorRoseAnalysis/MyNewPage.cs
+++ Widgets/ErrorRoseAnalysis/ErrorRoseAnalysis/MyNewPage.cs
@@ -8,23 +8,12 @@ namespace ErrorRoseAnalysis
     {
         public MyNewPage()
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    MyGrid()
-                }
-            };
+            Content = MyGrid();
         }
 
         private Grid MyGrid()
         {
-            Grid grid = new Grid {BackgroundColor = Color.Red, ColumnSpacing = 0, RowSpacing = 20};
-
-            grid.ColumnDefinitions = new ColumnDefinitionCollection
-            {
-                new ColumnDefinition{ Width=new GridLength(1, GridUnitType.Star) },
-                new ColumnDefinition{ Width=new GridLength(1, GridUnitType.Star) }
-            };
+            Grid grid = new Grid();
 
             grid.RowDefinitions = new RowDefinitionCollection
             {
@@ -32,37 +21,22 @@ namespace ErrorRoseAnalysis
                 new RowDefinition{ Height=new GridLength(1, GridUnitType.Star) }
             };
 
-            //Label a = new Label { Text = "A", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand };
-            //Label b = new Label { Text = "B", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand };
-            //Label c = new Label { Text = "C", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand };
-            //Label d = new Label { Text = "D", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand};
+            grid.ColumnDefinitions = new ColumnDefinitionCollection
+            {
+                new ColumnDefinition{ Width=new GridLength(1, GridUnitType.Star) },
+                new ColumnDefinition{ Width=new GridLength(1, GridUnitType.Star) }
+            };
 
-            BoxView a = new BoxView { BackgroundColor = Color.Cyan, HorizontalOptions = LayoutOptions.FillAndExpand};
-            BoxView b = new BoxView { BackgroundColor = Color.Magenta, HorizontalOptions = LayoutOptions.FillAndExpand };
-            BoxView c = new BoxView { BackgroundColor = Color.Yellow, HorizontalOptions = LayoutOptions.FillAndExpand };
-            BoxView d = new BoxView { BackgroundColor = Color.Black, HorizontalOptions = LayoutOptions.FillAndExpand };
+
+            BoxView a = new BoxView { BackgroundColor = Color.Cyan};
+            BoxView b = new BoxView { BackgroundColor = Color.Magenta };
+            BoxView c = new BoxView { BackgroundColor = Color.Yellow };
+            BoxView d = new BoxView { BackgroundColor = Color.Black };
 
             grid.Children.Add(a, 0, 0);
             grid.Children.Add(b, 0, 1);
             grid.Children.Add(c, 1, 0);
             grid.Children.Add(d, 1, 1);
-
-            //var grid = new Grid();
-
-            //grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            //grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            //grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            //grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-            //var topLeft = new Label { Text = "Top Left" };
-            //var topRight = new Label { Text = "Top Right" };
-            //var bottomLeft = new Label { Text = "Bottom Left" };
-            //var bottomRight = new Label { Text = "Bottom Right" };
-
-            //grid.Children.Add(topLeft, 0, 0);
-            //grid.Children.Add(topRight, 1, 0);
-            //grid.Children.Add(bottomLeft, 0, 1);
-            //grid.Children.Add(bottomRight, 1, 1);
 
             return grid;
         }
