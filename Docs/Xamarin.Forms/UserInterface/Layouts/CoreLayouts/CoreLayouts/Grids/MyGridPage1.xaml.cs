1edd9892f313fb4d5817c3ae319af899e85f0350
duncanjack <duncan@openword.co.uk>
2020-03-16 19:09:11 +0930

    Grid

---
+++ Docs/Xamarin.Forms/UserInterface/Layouts/CoreLayouts/CoreLayouts/Grids/MyGridPage1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoreLayouts.Grids
+{
+    public partial class MyGridPage1 : ContentPage
+    {
+        public MyGridPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
